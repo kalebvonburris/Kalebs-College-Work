64bcbb697b69f6c779b14e448bc1b903336ad48c
Kaleb Burris <kaleb.r.burris@gmail.com>
2023-09-11 20:06:23 -0800

    I messed around with rayon and discovered Big O is a lie

--- CS/CS_F311/HW01-Rust/src/main.rs
+++ CS/CS_F311/HW01-Rust/src/main.rs
@@ -1,65 +1,22 @@
-#[derive(Debug, Clone)]
-pub struct Player {
-    name: String,
-    username: String,
-    games_played: u64,
-}
+#![feature(test)]
 
-impl Player {
-    /// Three parameter constructor.
-    fn new(name: String, username: String, games_played: u64) -> Self {
-        Self {
-            name,
-            username,
-            games_played,
-        }
-    }
+extern crate test;
 
-    // Setters
-    fn set_name(&mut self, name: String) {
-        self.name = name;
-    }
+use test::Bencher;
 
-    fn set_username(&mut self, username: String) {
-        self.username = username;
-    }
+use rayon::prelude::*;
 
-    fn set_games_played(&mut self, games_played: u64) {
-        self.games_played = games_played;
-    }
-
-    /// Returns true if the player has played more
-    /// than one game.
-    fn is_active(&self) -> bool {
-        self.games_played > 0
-    }
-}
-
-impl Default for Player {
-    /// Default constructor.
-    /// Sets `name` and `username` to
-    /// "UNKNOWN" and `games_played to 0.
-    fn default() -> Self {
-        Player {
-            name: String::from("UNKNOWN"),
-            username: String::from("UNKNOWN"),
-            games_played: 0,
-        }
-    }
-}
-
-impl std::fmt::Display for Player {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}: {}, {}", self.name, self.username, self.games_played)
-    }
+fn sum_of_squares(input: &[i32]) -> i32 {
+    input.iter().map(|i| i * i).sum()
 }
 
 fn main() {
-    let name = String::from("Selena Gomeze");
-    let username = String::from("Mabel Mora");
-    let games_played = 15;
+    ()
+}
 
-    let player = Player::new(name, username, games_played);
+#[bench]
+fn test(b: &mut Bencher) {
+    let vals = &[16; 1000000];
 
-    println!("{}", player);
+    b.iter(|| sum_of_squares(vals))
 }
