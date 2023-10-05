f51e2ec357a4146d62e21912cf852259572bc8dc
Kaleb Burris <kaleb.r.burris@gmail.com>
2023-10-04 17:29:32 -0800

    Clean up

--- CS/CS_F311/Follow_Alongs/Merge_Sorting/target/debug/merge_sorting.d
+++
@@ -1,1 +0,0 @@
-/home/krburris/Desktop/Projects/repos/Kalebs-College-Work/CS/CS_F311/Follow_Alongs/Merge_Sorting/target/debug/merge_sorting: /home/krburris/Desktop/Projects/repos/Kalebs-College-Work/CS/CS_F311/Follow_Alongs/Merge_Sorting/src/main.rs
